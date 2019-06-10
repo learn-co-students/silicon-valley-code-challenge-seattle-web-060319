4a16b8daff4606711e8fb75f263612ba49ce5408
Jason Leach <jlspursfan@gmail.com>
2019-06-10 16:37:14 -0700

    Silicon Valley JL

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,68 @@
 class Startup
 
+  @@all = []
+
+  attr_accessor :name, :domain
+  attr_reader :founder
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot(domain, name)
+    @name = name
+    @domain = domain
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find { |startup| startup.founder == founder_name }
+  end
+
+  def self.domains
+    domain_arr = self.all.map { |startup| startup.domain }
+    return domain_arr.uniq
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    FundingRound.new(self, venture_capitalist, type, investment)
+  end
+
+  def all_rounds
+    FundingRound.all.select do |round|
+      round.startup == self
+    end
+  end
+
+  def num_funding_rounds
+    all_rounds.length
+  end
+
+  def total_funds
+    total = 0
+    all_rounds.each do |round|
+      total += round.investment
+    end
+    return total
+  end
+
+  def investors
+    array = all_rounds.map do |round|
+      round.venture_capitalist
+    end
+    return array.uniq
+  end
+
+  def big_investors
+    investors.select do |investor|
+      VentureCapitalist.tres_commas_club.include?(investor)
+    end
+  end
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,67 @@
 class VentureCapitalist
 
+  @@all = []
+
+  attr_accessor :cash
+  attr_reader :name
+
+  def initialize(name, cash)
+    @name = name
+    @cash = cash
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+    @cash -= investment
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |round|
+      round.venture_capitalist == self
+    end
+  end
+
+  def total_worth
+    investments = 0
+    funding_rounds.each do |round|
+      investments += round.investment
+    end
+    return investments + @cash
+  end
+
+  def self.tres_commas_club
+    tcc = @@all.select do |vc|
+      vc.total_worth > 1000000000
+    end
+    tcc.map do |member|
+      puts "#{member.name}: $#{member.total_worth}"
+    end
+  end
+
+  def portfolio
+    startups = funding_rounds.map { |round| round.startup }
+    return startups.uniq
+  end
+
+  def biggest_investment
+    investments = funding_rounds.map { |round| round.investment }
+    sorted = investments.sort { |a,b| b <=> a }
+    return sorted[0]
+  end
+
+  def invested(dom)
+    total = 0
+    funding_rounds.each do |round|
+      if round.startup.domain == dom
+        total += round.investment
+      end
+    end
+    return total
+  end
+
 end
