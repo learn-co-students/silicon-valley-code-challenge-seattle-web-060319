03b9e7138cd912b26c61ad9681bb1dd8e6ac2143
github username <github email address>
2019-06-10 21:36:35 -0700

    done ya bish

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,22 @@
 class FundingRound
 
+  attr_reader :startup, :venturecapitalist, :type, :investment
+
+  @@all = []
+
+  def initialize(startup, venturecapitalist, type, investment)
+    @startup = startup
+    @venturecapitalist = venturecapitalist
+    @type = type
+    @investment = investment
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
+
+
+# Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
